e9ee7ef0f0018d2c30789ee03089dd444b0ab0d5
Jabekah007 <70330339+Jabekah007@users.noreply.github.com>
2022-10-02 15:08:05 +0000

    added middleware

--- NodeJS/express/app.js
+++ NodeJS/express/app.js
@@ -1,8 +1,18 @@
 const express=require('express')
 const app = express();
 
+/*Middleware*/
+const reqFilter = (reqobject,resobject,next) =>{
+    console.log('reqFilter')
+    next();
+}
+app.use(reqFilter)
 app.get('/',(req,res) =>{
     res.send("Welcome to Stokka");
+});
+
+app.get('/home',(req,res) =>{
+    res.send("Welcome to Stokka homepage");
 });
 
 app.listen(4000,()=>{
